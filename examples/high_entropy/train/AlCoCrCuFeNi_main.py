d80b825fe62350747800ff23376fc0c7e7be5934
WangZ <wz17@mails.tsinghua.edu.cn>
2021-11-03 16:58:42 +0800

    Update

--- examples/high_entropy/train/AlCoCrCuFeNi_main.py
+++ examples/high_entropy/train/AlCoCrCuFeNi_main.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
 
     trn_idx = trainset.indices
     trn_mean = torch.tensor(trainset.dataset.PES)[trn_idx].mean().item()
-    # trn_std = torch.tensor(trainset.dataset.PES[trn_idx]).std().item()
+    # trn_std = torch.tensor(trainset.dataset.PES)[trn_idx].std().item()
 
     trainloader = DataLoader(trainset, batch_size=64, shuffle=True, collate_fn=_collate)
     valloader = DataLoader(valset, batch_size=64, shuffle=False, collate_fn=_collate)
@@ -45,7 +45,8 @@ if __name__ == '__main__':
     print(model)
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=3e-5, weight_decay=1e-5)
-    criterion = nn.L1Loss(reduction='sum')
+    criterion = nn.MSELoss(reduction='sum')
+    eval_ = nn.L1Loss(reduce='sum')
 
     gamma = 0.9
     best_log = inf
@@ -71,8 +72,8 @@ if __name__ == '__main__':
             loss_ = (1 - gamma) * e_loss_ + gamma * f_loss_
             loss_.backward()
 
-            loss_e += e_loss_.item()
-            loss_f += f_loss_.item()
+            loss_e += eval_(pred_e, e - trn_mean).item()
+            loss_f += eval_(pred_f, f).item()
 
             optimizer.step()
 
@@ -82,11 +83,11 @@ if __name__ == '__main__':
             val_g.ndata['x'].requires_grad = True
 
             pred_val_e = model(val_g, val_g.ndata['cell'])
-            val_loss_e += criterion(pred_val_e, val_e - trn_mean)
+            val_loss_e += eval_(pred_val_e, val_e - trn_mean)
 
             pred_val_f = - torch.autograd.grad(pred_val_e.sum(), 
                                                val_g.ndata['x'])[0]
-            val_loss_f += criterion(pred_val_f, val_f)
+            val_loss_f += eval_(pred_val_f, val_f)
 
         print('Epoch #{:01d} | MAE_E: {:.4f} | MAE_F: {:.4f} '
               '| Val MAE_E: {:.4f} | Val MAE_F: {:.4f}.'.format(epoch + 1, 
@@ -95,7 +96,7 @@ if __name__ == '__main__':
                                                                 val_loss_e / len(valset) / atom_nums, 
                                                                 val_loss_f / len(valset) / atom_nums / 3))
 
-        if best_log >= (val_loss_f / len(valset)):
-            best_log = (val_loss_f / len(valset))
+        if best_log >= (val_loss_f / len(valset) / atom_nums / 3):
+            best_log = val_loss_f / len(valset) / atom_nums / 3
             print('Save model...')
             torch.save(model.state_dict(), 'best-model.pt')
