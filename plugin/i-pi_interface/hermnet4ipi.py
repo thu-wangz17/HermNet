9afa6333d727d3f6439bd52b49a072f1be9941b9
WangZ <wz17@mails.tsinghua.edu.cn>
2021-12-24 21:33:59 +0800

    Add i-pi interface

---
+++ plugin/i-pi_interface/hermnet4ipi.py
@@ -0,0 +1,31 @@
+from ..ase_interface.hermnet4ase import NNCalculator
+from ase.calculators.socketio import SocketClient
+from ase.io.vasp import read_vasp
+
+def ipi_communicate(poscar: str, calc: NNCalculator, 
+                    host: str='localhost', port: int=1226, 
+                    mode: str='unix'):
+    atoms = read_vasp(poscar)    
+    atoms.set_calculator(calc)
+
+    assert mode in ['inet', 'unix']
+    
+    if mode == 'inet':
+        client = SocketClient(host=host, port=port)
+    elif mode == 'unix':
+        client = SocketClient(unixsocket=host)
+
+    client.run(atoms)
+
+
+# if __name__ == '__main__':
+#     import torch
+#     from HermNet.hermnet import HVNet
+
+#     rc = 5.
+#     device = torch.device('cuda')
+#     model = HVNet(elems=['C'], rc=rc, l=30, in_feats=128, molecule=False, md=True).to(device)
+
+#     calc = NNCalculator(model=model, model_path='./best-model.pt', trn_mean=-709., device_='cuda')
+
+#     ipi_communicate(poscar='./POSCAR', calc=calc)
---
+++ plugin/i-pi_interface/run.py
@@ -0,0 +1,59 @@
+import os
+import multiprocessing as mp
+from subprocess import Popen, PIPE
+import torch
+from ase.io.vasp import read_vasp
+from ase.io.xyz import write_xyz
+import argparse
+from HermNet.hermnet import HVNet
+from .hermnet4ipi import ipi_communicate
+from ..ase_interface.hermnet4ase import NNCalculator
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="HermNet works as a server for i-PI.")
+    parser.add_argument('-m', '--mode', help='The mode in i-PI.', choices=['unix', 'inet'], type=str)
+    parser.add_argument('-p', '--path', help='The path of the POSCAR.', type=str, required=True)
+    parser.add_argument('-s', '--model', help='The path of the saved model.', type=str, required=True)
+    parser.add_argument('-t', '--trnMean', help='The mean value of the dataset.', type=float, required=True)
+    parser.add_argument('-e', '--elems', help='Elements.', type=str, nargs='*', required=True)
+    parser.add_argument('-r', '--rc', help='Radius cutoff.', type=float, required=True)
+    args = parser.parse_args()
+
+    atoms = read_vasp(args.path)
+    comment = '# CELL(H):   ' \
+        + ' '.join(map(str, atoms.todict()['cell'].reshape(-1).tolist())) \
+            + '  cell{angstrom} Traj: positions{angstrom}'
+    
+    with open('init.xyz', 'w') as f:
+        write_xyz(f, images=[atoms], comment=comment)
+
+    pipe = Popen('pip show i-pi', shell=True, stdout=PIPE)
+    text = pipe.communicate()[0].decode().split('\n')
+
+    for line in text:
+        if 'Location' in line:
+            path = line.split(':')[1].split('/')
+            idx = path.index('anaconda3')
+            ipi_path = os.path.join('/'.join(path[:idx+1]), 'bin', 'i-pi')
+            break
+
+    rc = args.rc
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = HVNet(elems=args.elems, rc=rc, l=30, in_feats=128, molecule=False, md=True).to(device)
+
+    calc = NNCalculator(model=model, model_path=args.model, trn_mean=args.trnMean, device_='cuda')
+
+    def client():
+        os.system('python ' + ipi_path + ' input.xml', shell=True)
+
+
+    def server():
+        ipi_communicate(poscar=args.path, calc=calc)
+
+    proc_1 = mp.Process(target=client)
+    proc_2 = mp.Process(target=server)
+
+    proc_1.start()
+    proc_2.start()
+    proc_1.join()
+    proc_2.join()
